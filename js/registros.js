9309c94ca6210d091d33e0afc6a197d50595daed
lisocarvalho <43457575+lisocarvalho@users.noreply.github.com>
2018-11-22 15:14:39 -0200

    Update and rename presidente.js to registros.js

--- js/registros.js
+++ js/registros.js
@@ -2,40 +2,6 @@
 $('#dtPickerInicioVotacao').datetimepicker();
 $('#dtPickerFimVotacao').datetimepicker();
 
-function registraSecretario() {
-    if (verificaCondicoesInteragirSmartContract()) {
-        var frm = document.frmSecretario;
-        if (!web3.utils.isAddress(frm.secretario.value)) {
-            $("#statusSecretario").css("background-color", "Salmon");
-            $("#statusSecretario").html("Você deve informar um endereço Ethereum válido.");
-            return
-        }
-        console.log("registraSecretario - Enviando..." + frm.secretario.value);
-        $("#statusSecretario").text("Por favor, confirme o envio da transação no Metamask");
-        contract.methods.designarSecretario(frm.secretario.value).send({from: conta, gas: 3000000, value: 0})
-            .on('transactionHash', function(hash){
-                $("#statusSecretario").css("background-color", "yellow");
-                $("#statusSecretario").text("Transação enviada a rede do Ethereum. Aguarde enquanto ela é confirmada. Transaction hash: " + hash);
-            })
-            .on('confirmation', function(confirmationNumber, txReceipt){
-                if (confirmationNumber == 7) {
-                    if (txReceipt) {
-                        if (txReceipt.status == "0x1") {
-                            $("#statusSecretario").css("background-color", "LawnGreen");
-                            $("#statusSecretario").html("Registro salvo com sucesso no bloco: " + txReceipt.blockNumber + " - Transaction hash: " + txReceipt.transactionHash + " - Numero confirmações: " + confirmationNumber);            
-                        } else {
-                            $("#statusSecretario").css("background-color", "Salmon");
-                            $("#statusSecretario").html("Aconteceu um erro - Transaction hash: " + txReceipt.transactionHash + " - Status final: " + txReceipt.status);            
-                        }
-                    }
-                }
-            })
-            .on('error', function (err) {
-                $("#statusSecretario").css("background-color", "Salmon");
-                $("#statusSecretario").html("Aconteceu um erro: " + err);            
-            });
-    }
-}
 
 function registraInicioVotacao() {
     if (verificaCondicoesInteragirSmartContract()) {
