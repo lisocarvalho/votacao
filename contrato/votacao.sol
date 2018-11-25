db6003d20b0039f259e03c2216b8ef49b7746b7e
lisocarvalho <43457575+lisocarvalho@users.noreply.github.com>
2018-11-25 21:21:33 -0200

    alterações em todo o conteúdo

--- contrato/votacao.sol
+++ contrato/votacao.sol
@@ -2,16 +2,27 @@ pragma solidity 0.4.25;
 
 contract VotacaoContratacao {
     
-    string public PropostaContratacao;
-    address votante;
-    address presidente;
-    address resultado;
+    struct PropostaContratacao {
+        string Proposta;
+        address presidente;
+        address resultado;
+        
+    }
     
+    struct Votante {
+        address conta;
+        string identificacao;
+        uint numeroDeVotos;
+        bool existe;
+        
+    }
     
     modifier somentePresidente() {
         require (msg.sender==presidente, "Somente presidente pode realizar essa opção");
         _;
     }
+    
+    event Votou(address quemVotou, uint qualVoto);
     
     constructor () public {
         PropostaContratacao = "Contratação de Crédito em Banco";
@@ -24,22 +35,23 @@ contract VotacaoContratacao {
     
     function definirVotante (address qualVotante) public somentePresidente {
         require (qualVotante !=address (0), "Endereço de agente invalido");
-        votante = qualVotante;
+        Votante (qualVotante);
     }
     
-    function Votar (numeroProposta) public {
-        numeroProposta[votante] = true;
-        if(votante.votou)
-        throw;
-        votante.votou = true;
-        votante.votou = numeroProposta;
-        numeroProposta.resultado;
+    function Votar () public {
+        numeroProposta[Votante] = true;
+        require(numeroDeVotos <= 1, "Pode votar somente uma vez");
+        if(Votante.votou)
+            return (Votante.votou = true);
+        require (Votante.votou = numeroDeVotos);
+        numeroDeVotos.resultado;
         }
        
     
-    function getResultado () returns(numeroProposta){
-        return resultado;
-    }
+    function getresultado () {
+        return numeroDeVotos;
+        require (msg.value >= 0);
     }
     
 }
+    
